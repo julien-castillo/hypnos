a4b6640677e04e134c22609c6e522c2a836f378f
Julien Castillo <juliencastillo77@gmail.com>
2022-04-16 18:28:10 +0200

    fix routes addBooking

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, "index"])->name('home');
 
 Route::get('/addBooking', [PublicController::class, "booking"])->name('addBooking');
-
+Route::post('booking/check-availability', [BookingController::class, 'checkAvailability'])->name('checkAvailability');
+Route::post('booking/get-suites', [BookingController::class, 'getHotelSuites']);
 
 Route::get('/hotel/{hotel}/suites', [PublicController::class, "suites"])->name('suites');
 Route::get('/hotel/{hotel}/suites/{suite}', [PublicController::class, "suite"])->name('details');
@@ -103,8 +104,6 @@ Route::name('manager.')->prefix('manager')->middleware('manager')->group(functio
  */
 Route::name('booking.')->prefix('booking')->middleware('auth')->group(function () {
     Route::get('', [BookingController::class, 'index'])->name('index');
-    Route::post('/check-availability', [BookingController::class, 'checkAvailability'])->name('checkAvailability');
-    Route::post('/get-suites', [BookingController::class, 'getHotelSuites']);
     Route::get('create', [BookingController::class, "create"])->name("create");
     Route::post('create', [BookingController::class, 'store'])->name('store');
     Route::delete('{booking}', [BookingController::class, 'delete'])->name('delete');
