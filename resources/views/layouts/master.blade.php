0aa8423ba12ec52c9e6c4effed66fb22f7cbbf2e
Julien Castillo <juliencastillo77@gmail.com>
2022-04-18 16:43:44 +0200

    add logo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -68,9 +68,7 @@
                     <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                         <a href="/"
                            class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
-                            <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
-                                <use xlink:href="#bootstrap"/>
-                            </svg>
+                            <img class="logo" src="{{ asset('/img/logo-hypnos.png') }}" alt="Photo de l'hôtel">
                         </a>
 
                         <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
